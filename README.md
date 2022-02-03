3a9624a59445651a4d4ce40033adbe7d537b41dc
opensoars <sam_1700@hotmail.com>
2022-02-03 18:59:02 +0200

    republish, completely clean

---
+++ README.md
@@ -0,0 +1,36 @@
+# `sffs`
+
+Save File FTP Sync
+
+## What?
+
+Simple command line tool to help with remote FTP server "save and deploy".
+
+## Why?
+
+Because many existing tools are inconsistent with regards to their performance. They randomly drop SFTP connections, they can't create directories, they can't recursively parent directories, they can't upload on file change, etc etc.
+
+Sometimes me and the people I work with want to simply just work on a (simple and cheaply hosted) production enviroment straight from our local machine. (Oh no so dangerous oh no bla bla... Why don't you have \*insert one billion devops/ci/etc tools here\* set up for your project??! If you are like this, please realize that **ffs** is actually in the name of this project.)
+
+## How?
+
+When `sffs` gets started it tries to recursively mirror all **directories** in the current directory. This in order to be able to write **files**.
+
+When a **file** or **directory name** change gets detected it will upload that **file** or **directory** to the FTP server.
+
+Old **files** or **directories** don't get deleted from the FTP server when you rename them. (@TODO decide whether this is something to do)
+
+## Dependencies
+
+- Node.js & npm
+- lftp
+
+## Install
+
+`npm install -g sffs` (@TODO publish)
+
+## Use
+
+- Create a `.sffs` file from the directory you plan to run `sffs` from, according to the `.sffs_example` file in the root of this project.
+- Run the `sffs` command from the directory that you created a `.sffs` file in.
+- Enjoy the benefits of ultra fast production development/deployment (aka, providing value and making money) (use at your own risk :))
---
+++ bin/index.js
@@ -0,0 +1,109 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const { spawn } = require('child_process');
+
+const chokidar = require('chokidar');
+const dotenv = require('dotenv');
+
+const { CLIENT_NAME, SFTP_URL, DEBUG: _DEBUG, REMOTE_DIR } = dotenv.parse(
+  fs.readFileSync(`${process.cwd()}/.sffs`).toString()
+);
+const DEBUG = _DEBUG === 'true';
+
+if (CLIENT_NAME) console.log('\nclient:', CLIENT_NAME, '\n');
+
+chokidar
+  .watch('.', {
+    // @TODO get (extend?) this from .sffs
+    ignored: ['node_modules', /(^|[\/\\])\../]
+  })
+  .on('all', (event, path) => {
+    if (event === 'addDir') {
+      if (path !== '.') {
+        handleAddDir(path);
+      }
+    } else if (event === 'change') {
+      handleChange(path);
+    }
+  });
+
+function handleAddDir(path) {
+  console.log('+', path);
+
+  const dirToCreate = `${path}`;
+
+  // @TODO have one function for the lftp spawn
+  const lftpArgs = [
+    DEBUG ? '-d' : '',
+    '-e',
+    // @TODO get (extend?) from .sffs
+    `set net:max-retries 10; set net:timeout 5;` +
+      `set net:reconnect-interval-multiplier 1;` +
+      `set net:reconnect-interval-base 5;` +
+      `cd ${REMOTE_DIR};` +
+      `mkdir -p ${dirToCreate};` +
+      `&& exit`,
+    SFTP_URL
+  ];
+
+  const lftp = spawn('lftp', lftpArgs);
+
+  lftp.stdout.on('data', data => {
+    if (DEBUG) console.log(`+ - stdout: ${data}`);
+  });
+
+  lftp.stderr.on('data', data => {
+    if (DEBUG) console.error(`+ - stderr: ${data}`);
+  });
+
+  lftp.on('close', code => {
+    if (code === 0) {
+      console.log('+ fullfilled:', path);
+    } else {
+      console.log('+ rejected:', path);
+    }
+  });
+}
+
+function handleChange(path) {
+  console.log('^', path);
+
+  const splitPath = path.split('/');
+
+  const remoteDir =
+    splitPath.length === 1
+      ? REMOTE_DIR
+      : `${REMOTE_DIR}/${splitPath.slice(0, -1).join('/')}`;
+
+  // @TODO have one function for the lftp spawn
+  const lftpArgs = [
+    DEBUG ? '-d' : '',
+    '-e',
+    // @TODO get (extend?) from .sffs
+    `set net:max-retries 10; set net:timeout 5; ` +
+      `set net:reconnect-interval-multiplier 1; ` +
+      `set net:reconnect-interval-base 5; ` +
+      `put -P -O ${remoteDir} ${path};` +
+      `&& exit`,
+    SFTP_URL
+  ];
+
+  const lftp = spawn('lftp', lftpArgs);
+
+  lftp.stdout.on('data', data => {
+    if (DEBUG) console.log(`^ stdout: ${data}`);
+  });
+
+  lftp.stderr.on('data', data => {
+    if (DEBUG) console.error(`^ stderr: ${data}`);
+  });
+
+  lftp.on('close', code => {
+    if (code === 0) {
+      console.log('^ fullfilled:', path);
+    } else {
+      console.log('^ rejected:', path);
+    }
+  });
+}
